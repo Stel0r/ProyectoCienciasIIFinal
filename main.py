728612db5466bde12be0e1ed8a5d08cd4fe10055
Harrinson Toledo Gonzalez <75257608+HarrinsonToledo@users.noreply.github.com>
2023-10-03 14:25:52 -0500

    hash 2

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import sys
 from PyQt5 import QtWidgets as QtW
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import QApplication
 
 from ventanaIndices import IndicesView
+from HashDirectory.ventanaHash import HashView
+
 
 def window():
     app = QApplication(sys.argv)
     w = QtW.QMainWindow()
-    w.setGeometry(100,100,1080,720)
-
+    w.setGeometry(100, 100, 1080, 720)
 
     panelOpciones = IndicesView(None)
+    panelHash = HashView(None)
 
     ventanaTabs = QtW.QTabWidget(w)
-    ventanaTabs.setGeometry(0,0,1080,720)
-    ventanaTabs.addTab(panelOpciones,"Indices")
-    ventanaTabs.setStyleSheet("QTabBar::Tab{background-color:#F3F3F3;} QTabBar::Tab::hover{background-color:#E5E5E5;} QTabBar::Tab::selected{background-color:#CCCCCC;} ")
+    ventanaTabs.setGeometry(0, 0, 1080, 730)
+    ventanaTabs.addTab(panelOpciones, "Indices")
+    ventanaTabs.addTab(panelHash, "Hash")
+    ventanaTabs.setStyleSheet(
+        "QTabBar::Tab{background-color:#F3F3F3;} QTabBar::Tab::hover{background-color:#E5E5E5;} QTabBar::Tab::selected{background-color:#CCCCCC;} ")
 
     w.show()
     app.exec()
 
-    
-	
+
 if __name__ == '__main__':
     window()
