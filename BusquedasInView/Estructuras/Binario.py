ab037b6e316954e23bf2ff6d86cfea117fb1b24b
Diego Gamez <diego.felipe.gamez@gmail.com>
2023-10-05 22:34:07 -0500

    commit busqueda binaria

--- BusquedasInView/Estructuras/Binario.py
+++ BusquedasInView/Estructuras/Binario.py
@@ -7,17 +7,18 @@ class Binario(EstructuraInterna):
 
     def busqueda(self, valor):
         tamaño = len(self.matriz)
+        mat = self.matriz
         while True:
             central = round(tamaño / 2) - 1
-            if valor == self.matriz[central]:
+            if valor == mat[central]:
                 return "(Binario) el registro numerico "+str(valor)+" se encontro en "+str(central+1)
-            elif valor < self.matriz[central] and not tamaño <= 1:
-                self.matriz = self.matriz[:central]
-            elif valor > self.matriz[central] and not tamaño <= 1:
-                self.matriz = self.matriz[central + 1:]
+            elif valor < mat[central] and not tamaño <= 1:
+                mat = mat[:central]
+            elif valor > mat[central] and not tamaño <= 1:
+                mat = mat[central + 1:]
             elif tamaño <= 1:
                 return f"(Binario) el registro "+str(valor)+" no se encontro"
-            tamaño = len(self.matriz)
+            tamaño = len(mat)
 
     def ingresarDato(self, valor):
         if (len(self.matriz) < self.rango):
@@ -25,7 +26,8 @@ class Binario(EstructuraInterna):
                 if self.matriz[x] == valor:
                     return "el registro ya se encuentra en la estructura"
                 if self.matriz[x] > valor:
-                    self.matriz.insert(x,valor)
+                    self.matriz.append(valor)
+                    self.matriz = sorted(self.matriz)
                     return "el registro "+str(valor)+" ha sido ingresado con exito"
             self.matriz.append(valor)
             return "el registro "+str(valor)+" ha sido ingresado con exito al final"
